771feeb396baff41ee607f1173e096bc167dcc94
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-05-17 00:09:59 +0900

    [level 1] Title: 추억 점수, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,14 @@
+function solution(name, yearning, photo) {
+    var answer = [];
+    for(let a = 0;a<photo.length;a++){
+        answer.push(0);
+        for(let b = 0;b<photo[a].length;b++){
+            for(let c = 0;c<name.length;c++){
+                if(name[c] == photo[a][b]){
+                    answer[a]+=yearning[c];
+                }
+            }
+        }
+    }
+    return answer;
+}
