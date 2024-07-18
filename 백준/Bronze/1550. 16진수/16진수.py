83dd88a701136b69ab91f252843de296f3137b33
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-07-18 20:13:14 +0900

    [Bronze II] Title: 16진수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.py
@@ -0,0 +1,16 @@
+a= list(input())
+b = 16
+#-55
+ans = 0
+for i in range(len(a)-1, -1, -1):
+    try1 = len(a)-i-1
+    #print(i)
+    if(ord(a[i]) <= 57):
+        i = int(a[i])
+        #print(i*(b**try1))
+        ans+=i*(b**try1)
+    else:
+        i = (ord(a[i])-55)*(b**try1)
+        #print(i)
+        ans+=i
+print(ans)
