b8798b985d847fcfffb7cb2ab4c11c6f37aebb54
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-08-21 20:14:03 +0900

    [Silver III] Title: 프린터 큐, Time: 56 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,24 @@
+from collections import deque
+a = int(input())
+for i in range(a):
+    b,c = map(int ,input().split())
+    li1 = list(map(int, input().split()))
+    li1 = deque(li1)
+    pre = c
+    Max = max(li1)
+    d = li1.popleft()
+    ans = 1
+    while pre!=0 or d != Max:
+        #print(li1, pre, ans)
+        if d == Max:
+           ans+=1
+           Max = max(li1)
+           pre-=1
+        else:
+            if pre == 0:
+                pre = len(li1)
+            else:
+                pre-=1
+            li1.append(d)
+        d = li1.popleft()
+    print(ans)
