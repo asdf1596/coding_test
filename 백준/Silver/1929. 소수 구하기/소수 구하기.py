e5d93da13e248380d73420046682d82ad247797a
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-08-26 19:18:30 +0900

    [Silver III] Title: 소수 구하기, Time: 168 ms, Memory: 117392 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,13 @@
 import sys
-from collections import deque
-a,b = map(int, sys.stdin.readline().split())
-li1 = [i for i in range(a,b+1) if (i%2==1 or i==2) and i!=1]
-li1 = deque(li1)
-if a==b and b==1:
-    print("")
-else:
-    for i in range(3,int(b**(1/2))+1,2):
-        for j in range(len(li1)):
-            c = li1.popleft()
-            if c%i!=0 or c==i:
-                li1.append(c)
-    for i in range(len(li1)):
-        print(li1.popleft())
+import math
+a, b = map(int, sys.stdin.readline().split())
+li1 = [True] * (b - a + 1)
+if a == 1:
+    li1[0] = False
+for i in range(2, int(math.sqrt(b)) + 1):
+    start = max(i * i, (a + i - 1) // i * i)
+    for j in range(start, b + 1, i):
+        li1[j - a] = False
+for i in range(a, b + 1):
+    if li1[i - a]:
+        print(i)
