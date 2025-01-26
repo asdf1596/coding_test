676db056fd3a97d1eb3d7ac7330b2374435adb57
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2025-01-26 14:18:20 +0900

    [Bronze III] Title: 주사위 게임, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -0,0 +1,16 @@
+case = int(input())
+ans = 0
+for _ in range(case):
+    a, b, c = map(int, input().split())
+    
+    if a == b == c:
+        ans = max(ans, 10000+a*1000)
+    elif a == b:
+        ans = max(ans, 1000+a*100)
+    elif a == c:
+        ans = max(ans, 1000+a*100)
+    elif b == c:
+        ans = max(ans, 1000+b*100)
+    else:
+        ans = max(ans, 100 * max(a,b,c))
+print(ans)
