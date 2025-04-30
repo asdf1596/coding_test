b2734ed24b0a7a1f0cfb9a82ece3b4e8f50f62af
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2025-04-30 18:48:46 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    _ = input()
+    N = int(input())
+    candy = 0
+    for _ in range(N):
+        candy += int(input())
+    if candy % N == 0:
+        print("YES")
+    else:
+        print("NO")
